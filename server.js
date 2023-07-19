66189970e3551e215e3f05461ea0f8f86657bd15
Barchuk Andrii <abarchuk1976@gmail.com>
2023-07-19 15:38:11 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const { MONGO_URL, PORT = 3000 } = process.env;
+
+mongoose.connect(MONGO_URL).then(()=> {
+	console.log("Mongo DB successfully connected..")
+}).catch((err) => {
+	console.error(err);
+
+	process.exit(1);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running. Use our API on port: ${PORT}`);
+})
